3abae41430c2ad096fa3999639d99425fc7fb95e
Hasib <hasibfire24@gmail.com>
2024-05-06 17:35:01 +0600

    .

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div className="container">
+                {/* Logo and Links section */}
+                <div>
+                    <a href="#">
+                        Eshop
+                    </a>
+                </div>
+                {/* Navbar Right section */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
