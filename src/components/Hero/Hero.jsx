4e871794a0d60f978bed0ab2ba296631fbac2598
Chipsii <hasibfire24@gmail.com>
2024-05-19 19:24:02 +0600

    finish category section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import Image1 from '../../assets/hero/headphone.png';
 import Image2 from '../../assets/category/vr.png';
 import Image3 from '../../assets/category/macbook.png';
+import Button from '../shared/Button';
 
 const HeroData = [
     {
@@ -58,17 +59,21 @@ const Hero = () => {
                                     <div className='grid grid-cols-1 sm:grid-cols-2'>
                                         {/* text content section */}
                                         <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
-                                            <h1>{data.subtitle}</h1>
-                                            <h1>{data.title}</h1>
-                                            <h1>{data.title2}</h1>
+                                            <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                                            <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px]  font-bold'>{data.title2}</h1>
                                             <div>
-                                                <button>Shop Now</button>
+                                                <Button
+                                                text="Shop By Category"
+                                                bgColor="bg-primary"
+                                                textColor="text-white"
+                                                />
                                             </div>
                                         </div>
                                         {/* img section */}
                                         <div className='order-1 sm:order-2'>
                                             <div>
-                                                <img src={data.img} alt="" className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+                                                <img src={data.img} alt="" className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40' />
                                             </div>
                                         </div>
                                     </div>
